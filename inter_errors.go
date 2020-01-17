690d94b2e53fc0ea22b81de4e42e7729cfbff8cc
eco9999 <emrecanocak@windowslive.com>
2020-01-17 18:42:10 +0300

    missing _ERROR added

--- inter_errors.go
+++ inter_errors.go
@@ -2,22 +2,22 @@ package jsoninterpreter
 
 import "errors"
 
-func NULL_PATH_ERROR() 			error {return errors.New("Error: Path can't be null. ERROR_CODE:00")}
-func NULL_NEW_VALUE_ERROR() 	error {return errors.New("Error: New value can't be null. ERROR_CODE:01")}
-func INDEX_EXPECTED_ERROR() 	error {return errors.New("Error: Index expected, got key value. ERROR_CODE:02")}
-func KEY_EXPECTED_ERROR() 		error {return errors.New("Error: Key expected, got index. ERROR_CODE:03")}
-func OBJECT_EXPECTED_ERROR() 	error {return errors.New("Error: Last path must be pointed at an object. ERROR_CODE:04")}
-func ARRAY_EXPECTED_ERROR() 	error {return errors.New("Error: Last path must be pointed at an array. ERROR_CODE:05")}
-func INDEX_OUT_OF_RANGE_ERROR() error {return errors.New("Error: Index out of range. ERROR_CODE:06")}
-func KEY_NOT_FOUND_ERROR() 		error {return errors.New("Error: Key not found. ERROR_CODE:07")}
-func BAD_JSON_ERROR() 			error {return errors.New("Error: Bad JSON format. ERROR_CODE:08")}
-func BAD_KEY_ERROR() 			error {return errors.New("Error: Key value can't contain quote symbol. ERROR_CODE:09")}
-func KEY_ALREADY_EXIST() 		error {return errors.New("Error: Key already exist. Use Set() function to change. ERROR_CODE:10")}
-func CAST_INT_ERROR() 			error {return errors.New("Cast Error: Cast to int error. ERROR_CODE:11")}
-func CAST_FLOAT_ERROR() 		error {return errors.New("Cast Error: Cast to float error. ERROR_CODE:12")}
-func CAST_BOOL_ERROR() 			error {return errors.New("Cast Error: Cast to bool error. ERROR_CODE:13")}
-func CAST_STRING_ARRAY_ERROR() 	error {return errors.New("Cast Error: Cast to []string error. ERROR_CODE:14")}
-func CAST_INT_ARRAY_ERROR() 	error {return errors.New("Cast Error: Cast to []int error. ERROR_CODE:15")}
-func CAST_FLOAT_ARRAY_ERROR() 	error {return errors.New("Cast Error: Cast to []float error. ERROR_CODE:16")}
-func CAST_BOOL_ARRAY_ERROR() 	error {return errors.New("Cast Error: Cast to []bool error. ERROR_CODE:17")}
-func EMPTY_ARRAY_ERROR() 		error {return errors.New("Error: Array is empty. ERROR_CODE:18")}
+func NULL_PATH_ERROR() 			error {return errors.New("ERROR_CODE:00 Error: Path can't be null. ")}
+func NULL_NEW_VALUE_ERROR() 	error {return errors.New("ERROR_CODE:01 Error: New value can't be null. ")}
+func INDEX_EXPECTED_ERROR() 	error {return errors.New("ERROR_CODE:02 Error: Index expected, got key value. ")}
+func KEY_EXPECTED_ERROR() 		error {return errors.New("ERROR_CODE:03 Error: Key expected, got index. ")}
+func OBJECT_EXPECTED_ERROR() 	error {return errors.New("ERROR_CODE:04 Error: Last path must be pointed at an object. ")}
+func ARRAY_EXPECTED_ERROR() 	error {return errors.New("ERROR_CODE:05 Error: Last path must be pointed at an array. ")}
+func INDEX_OUT_OF_RANGE_ERROR() error {return errors.New("ERROR_CODE:06 Error: Index out of range. ")}
+func KEY_NOT_FOUND_ERROR() 		error {return errors.New("ERROR_CODE:07 Error: Key not found. ")}
+func BAD_JSON_ERROR() 			error {return errors.New("ERROR_CODE:08 Error: Bad JSON format. ")}
+func BAD_KEY_ERROR() 			error {return errors.New("ERROR_CODE:09 Error: Key value can't contain quote symbol. ")}
+func KEY_ALREADY_EXIST_ERROR()	error {return errors.New("ERROR_CODE:10 Error: Key already exist. Use Set() function to change. ")}
+func CAST_INT_ERROR() 			error {return errors.New("ERROR_CODE:11 Cast Error: Cast to int error. ")}
+func CAST_FLOAT_ERROR() 		error {return errors.New("ERROR_CODE:12 Cast Error: Cast to float error. ")}
+func CAST_BOOL_ERROR() 			error {return errors.New("ERROR_CODE:13 Cast Error: Cast to bool error. ")}
+func CAST_STRING_ARRAY_ERROR() 	error {return errors.New("ERROR_CODE:14 Cast Error: Cast to []string error. ")}
+func CAST_INT_ARRAY_ERROR() 	error {return errors.New("ERROR_CODE:15 Cast Error: Cast to []int error. ")}
+func CAST_FLOAT_ARRAY_ERROR() 	error {return errors.New("ERROR_CODE:16 Cast Error: Cast to []float error. ")}
+func CAST_BOOL_ARRAY_ERROR() 	error {return errors.New("ERROR_CODE:17 Cast Error: Cast to []bool error. ")}
+func EMPTY_ARRAY_ERROR() 		error {return errors.New("ERROR_CODE:18 Error: Array is empty. ")}
