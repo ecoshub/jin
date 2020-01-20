cfbaa73a824f24b1db5a7477f1834789047424d1
eco9999 <emrecanocak@windowslive.com>
2020-01-21 00:03:31 +0300

    New core testing & test-case generation functions

---
+++ core_test.go
@@ -0,0 +1,47 @@
+package jint
+
+import (
+	"testing"
+	"strings"
+	test "jint/test"
+)
+
+var json []byte
+var paths [][]string
+var values []string
+
+func TestGetInit(t *testing.T){
+	str, err := test.ExecuteNode()
+	if err != nil {
+		t.Errorf("Init Error E:%v , S:%v\n", err, str)
+	}
+
+	json = Flatten(test.ReadFile("test/test-json.json"))
+	newPaths := strings.Split(string(test.ReadFile("test/test-json-paths.json")), "\n")
+	newValues := strings.Split(string(test.ReadFile("test/test-json-values.json")), "\n")
+
+	for i,val := range newValues {
+		if val != "" && newPaths[i] != "" {
+			paths = append(paths, ParseArray(newPaths[i]))
+			values = append(values, val)
+		}
+	}
+}
+
+func TestGet(t *testing.T){
+	if len(paths) != len(values) {
+		t.Errorf("Paths and Values length not equal. %v %v \n", len(paths), len(values))
+	}
+	if len(paths) == 0 {
+		t.Errorf("Paths and Values length is zero.\n")
+	}
+	for i, _ := range paths {
+		value, done := Get(json, paths[i]...)
+		if done != nil {
+			t.Errorf("Total Fail, path:%v\n", paths[i])
+		}
+		if string(value) != StripQuotes(values[i]) {
+			t.Errorf("Fail, not same answer path:%v\n, got:\t\t>%v<\n, expected:\t>%v<\n", paths[i], string(value), values[i] )
+		}
+	}
+}
