2f5837921023afa8bf8b3fec995ef3dad014933f
eco9999 <emrecanocak@windowslive.com>
2020-02-05 21:31:26 +0300

    small test-case changes

--- benchmark/parse_get_benchmark_test.go
+++ benchmark/parse_get_benchmark_test.go
@@ -76,10 +76,10 @@ func BenchmarkJsoniteratorGetLarge(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		prs := jsoniter.Get(LargeFixture)
-		prs.Get("users", 0, "id")
-		prs.Get("users", 31, "id")
-		prs.Get("topics", "topics", 0, "id")
-		prs.Get("topics", "topics", 29, "id")
+		prs.Get("users", 0)
+		prs.Get("users", 31)
+		prs.Get("topics", "topics", 0)
+		prs.Get("topics", "topics", 29)
 	}
 }
 
@@ -88,10 +88,10 @@ func BenchmarkFastjsonGetLarge(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		var p fastjson.Parser
 		prs, _ := p.ParseBytes(LargeFixture)
-		prs.Get("users", "0", "id")
-		prs.Get("users", "31", "id")
-		prs.Get("topics", "topics", "0", "id")
-		prs.Get("topics", "topics", "29", "id")
+		prs.Get("users", "0")
+		prs.Get("users", "31")
+		prs.Get("topics", "topics", "0")
+		prs.Get("topics", "topics", "29")
 	}
 }
 
@@ -99,9 +99,9 @@ func BenchmarkJintParseGetLarge(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		prs, _ := jin.Parse(LargeFixture)
-		prs.Get("users", "0", "id")
-		prs.Get("users", "31", "id")
-		prs.Get("topics", "topics", "0", "id")
-		prs.Get("topics", "topics", "29", "id")
+		prs.Get("users", "0")
+		prs.Get("users", "31")
+		prs.Get("topics", "topics", "0")
+		prs.Get("topics", "topics", "29")
 	}
 }
