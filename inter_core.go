89144f16e6109f0bc2594c8336d40c7c8b887bbb
eco9999 <emrecanocak@windowslive.com>
2020-02-06 11:35:57 +0300

    godoc try #3

--- inter_core.go
+++ inter_core.go
@@ -1,3 +1,11 @@
+// Copyright (c) 2020 eco
+// license that can be found in the LICENSE file.
+
+/*
+Package jin is a comprehensive JSON manipulation tools bundle
+It provides parse, interpert, build and format tools.
+Third party packages only used for benchmark. No dependency need for core functions, 
+*/
 package jin
 
 import "strconv"
