b892d3b5323d8c1e87e6dfb3a783e35e16736706
eco9999 <emrecanocak@windowslive.com>
2020-02-06 11:52:22 +0300

    godoc try #3

--- example_Add_test.go
+++ example_Add_test.go
@@ -2,12 +2,29 @@ package jin
 
 import "fmt"
 
-func ExampleAdd() {
-    fmt.Println("hello")
-    // Output: hello
+func Example() {
+
 }
 
-func ExampleParser_Add() {
-    fmt.Println("hi")
-    // Output: hi
+// Add() adds a value to an array
+// it must point to an array
+// otherwise it will provide an error message through 'err' variable
+func ExampleAdd() {
+    var err error
+	var exampleJSON []byte = []byte(`{"user":"eco","year":2020,"links":["https://github.com/ecoshub"]}`)
+	
+	var newLink []byte = []byte(`"https://dev.to/eco9999"`)
+
+	exampleJSON, err = jin.Add(exampleJSON, newLink, "links")
+	if err != nil {
+		fmt.Println(err.Error())
+		return 
+	}
+	fmt.Println(string(exampleJSON))
+    // Output: {"user":"eco","year":2020,"links":["https://github.com/ecoshub","https://dev.to/eco9999"]}
 }
+
+// func ExampleParser_Add() {
+//     fmt.Println("hi")
+//     // Output: hi
+// }
