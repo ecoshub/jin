36ab42598513884b090cbb1d7e75efa7ba5be68a
eco9999 <emrecanocak@windowslive.com>
2020-02-06 12:19:28 +0300

    godoc Delete()

--- package_description.go
+++ package_description.go
@@ -2,8 +2,7 @@
 // license that can be found in the LICENSE file.
 
 /*
-Package jin is a comprehensive JSON manipulation tools bundle
-.It provides parse, interpert, build and format tools.
+Package jin is a comprehensive JSON manipulation tools bundle.It provides parse, interpert, build and format tools.
 Third party packages only used for benchmark. No dependency need for core functions, 
 */
 package jin
