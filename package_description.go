aaf2bd3efb4058d37e2fc5d36333c7ae6ced9817
eco9999 <emrecanocak@windowslive.com>
2020-02-06 21:54:48 +0300

    godoc quick-start section done

--- package_description.go
+++ package_description.go
@@ -1,29 +1,29 @@
 /*
-
 **UNDER CONSTRUCTION**
 
 Copyright (c) 2020 eco.
 
-license that can be found in the LICENSE file.
+License that can be found in the LICENSE file.
 
 
 Package Jin is a comprehensive JSON manipulation tools bundle.
+All functions tested with random data against NODEJS.
+All test-path and test-value cases created automatically with NODEJS.
 It provides parse, interpret, build and format tools.
-Third party packages only used for benchmark. No dependency need for core functions.
+Third-party packages only used for the benchmark. No dependency need for core functions.
 
-There are four types of tools in  Jin:
+Benchmarked against.
 
-- Interpreter
+	"github.com/buger/jsonparser" (with interpreter)
+	"github.com/valyala/fastjson" (with parser)
+	"github.com/json-iterator/go" (with parser)
 
-- Parser
+In conclusion the results of the benchmark 'Jin' was the fastest and lightweight package
 
-- Builder
+For more information take a look at BENCHMARK section.
 
-- Formater
 
-PARSER AND INTERPRETER
-
-INTERPRETER
+PARSER VS INTERPRETER
 
 Major difference between parsing and interpreting is,
 parser has to read all data before answer your needs.
@@ -31,6 +31,10 @@ On the other hand interpreter reads up to find the path you need.
 
 Once the parse is complete you can access any data with no time.
 But there is a time cost to parse data, and this cost can increase as data content grows.
+
+If you need to access all keys of a JSON then I am simply recommend you to user Parser.
+But if you need to access some keys of a JSON I strongly refoment you to use Interperter, it will be much faster than parser. 
+
 
 QUICK START
 
@@ -41,7 +45,7 @@ First let's look at function parameters.
 
 	// All interperter functions need one JSON as byte slice format. 
 	json := []byte(`{"git":"ecoshub","repo":{"id":233809925,"name":"ecoshub/jin"}}`)
-	
+
 	// And most of them needs a path value for navigate.
 	path := []string{"repo", "name"}
 
@@ -51,53 +55,70 @@ Get function returns the value that path has pointed.
 
 	value, err := jin.Get(json, path...)
 	if err != nil {
-		return err
+	return err
 	}
 
 path value can be a string slice or hard coded
 
 	value, err := jin.Get(json, "repo", "name")
 	if err != nil {
-		return err
+	return err
 	}
 
 Get() function return type is byte slice.
 
 All variations of return types are implemented as different functions.
 
-for example. if you need 'value' as string.
+For example. If you need 'value' as string,
 then you can use GetString() like this.
 
 	value, err := jin.GetString(json, "repo", "name")
 	if err != nil {
-		return err
+	return err
 	}
 
-All interpreter functions (except function variations) has own example provided in godoc.
+PARSER
 
-The Interpreter has 36 functions.
+Parser is another alternative for manipulation JSON.
+We recommend to use this structure when you need to access all or most of the keys in the JSON.
 
-Other Importent functions
+Parser constructor need only one parameter.
 
-- func Get(json []byte, path ...string) ([]byte, error)
+	// Parser constructor function jin.Parse() need one JSON as byte slice format. 
+	json := []byte(`{"git":"ecoshub","repo":{"id":233809925,"name":"ecoshub/jin"}}`)
 
-- func Add(json []byte, value []byte, path ...string) ([]byte, error)
+Lets Parse it with Parse function.
 
-- AddKeyValue
+	prs, err := jin.Parse(json)
+	if err != nil {
+	return err
+	}
 
-- Set
+Let's take the function Parser.Get()
 
-- SetKey
+	value, err := prs.Get("repo")
+	if err != nil {
+	return err
+	}
 
-- Delete
+*About path value look above. 
 
-- Insert
+Parser.Get() function return type is byte slice like Get() function of interpreter.
 
-- IterateArray
+All variations of return types are implemented as different functions.
 
-- IterateKeyValue
+For example. If you need 'value' as string.
+Then you can use Parser.GetString() like this.
 
+	value, err := prs.GetString("repo")
+	if err != nil {
+	return err
+	}
 
+All interpreter/parser functions (except function variations line GetString()) has own example provided in godoc.
 
+Other important functions of interpreter/parser. 
+
+- func Add(), AddKeyValue(), Set(), SetKey() Delete(), Insert(), IterateArray(), IterateKeyValue()
 */
 package jin
