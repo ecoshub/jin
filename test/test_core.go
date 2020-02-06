ea5f022a73a5015a3242cf2d2821e6384ff5de34
eco9999 <emrecanocak@windowslive.com>
2020-02-06 23:06:39 +0300

    standart test-case change

--- test/test_core.go
+++ test/test_core.go
@@ -10,6 +10,9 @@ import (
 	"strings"
 )
 
+// WriteFile is NOT FOR PUBLIC USAGE.
+// This function is created for test-case creation automation
+// Please do not change any thing. And do not use them.
 func WriteFile(filedir string, buffer []byte) {
 	err := ioutil.WriteFile(filedir, buffer, 0666)
 	if err != nil {
@@ -17,6 +20,9 @@ func WriteFile(filedir string, buffer []byte) {
 	}
 }
 
+// ReadFile is NOT FOR PUBLIC USAGE.
+// This function is created for test-case creation automation
+// Please do not change any thing. And do not use them.
 func ReadFile(dir string) []byte {
 	buff, err := ioutil.ReadFile(dir)
 	if err != nil {
@@ -26,6 +32,9 @@ func ReadFile(dir string) []byte {
 	return buff
 }
 
+// ExecuteNode is NOT FOR PUBLIC USAGE.
+// This function is created for test-case creation automation
+// Please do not change any thing. And do not use them.
 func ExecuteNode(par string) (string, error) {
 	cmd := exec.Command("node", "test/test-case-creator.js", par)
 	var out bytes.Buffer
@@ -37,6 +46,9 @@ func ExecuteNode(par string) (string, error) {
 	return out.String(), nil
 }
 
+// Dir is NOT FOR PUBLIC USAGE.
+// This function is created for test-case creation automation
+// Please do not change any thing. And do not use them.
 func Dir(dir string) []string {
 	files := make([]string, 0, 100)
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
@@ -52,11 +64,17 @@ func Dir(dir string) []string {
 	return files[1:]
 }
 
+// GetCurrentDir is NOT FOR PUBLIC USAGE.
+// This function is created for test-case creation automation
+// Please do not change any thing. And do not use them.
 func GetCurrentDir() string {
 	wd, _ := os.Getwd()
 	return wd
 }
 
+// Sep is NOT FOR PUBLIC USAGE.
+// This function is created for test-case creation automation
+// Please do not change any thing. And do not use them.
 func Sep() string {
 	return string(os.PathSeparator)
 }
